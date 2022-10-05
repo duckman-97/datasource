6e1111bb5ac7298eb0a5dac679676bf862de4487
duckman-97 <72430680+duckman-97@users.noreply.github.com>
2022-10-05 17:08:08 +0900

    datasource

---
+++ src/main/java/sbp/Ctxtst7Application.java
@@ -0,0 +1,13 @@
+package sbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ctxtst7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ctxtst7Application.class, args);
+	}
+
+}
---
+++ src/test/java/sbp/Ctxtst7ApplicationTests.java
@@ -0,0 +1,13 @@
+package sbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ctxtst7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
